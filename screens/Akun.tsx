d77a7b2cfad625b749d34488b6a6d115d60eda43
serlyset <serlysetyani@student.telkomuniversity.ac.id>
2020-10-23 00:08:36 +0700

    add header akun

--- screens/Akun.tsx
+++ screens/Akun.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text } from '../components/Themed';
+import Header from '../components/Header';
 
-export default function TabOneScreen() {
+export default function TabTwoScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab One</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <EditScreenInfo path="/screens/TabOneScreen.js" />
-        </View>
+        <SafeAreaView>
+            <Header textHeader={'Akun'} />
+            <View style={styles.container}>
+            </View>
+        </SafeAreaView>
+
     );
 }
 
