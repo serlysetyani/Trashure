35882d0157b10e6fb182ff0d2cc4eebd84338872
serlyset <serlysetyani@student.telkomuniversity.ac.id>
2020-11-07 16:36:08 +0700

    add: navigation tab on notifikasi screen

---
+++ screens/DetailTransaksi.tsx
@@ -0,0 +1,33 @@
+import { StackScreenProps } from '@react-navigation/stack';
+import * as React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function DetailTransaksi({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Hola</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+    },
+    linkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+});
--- screens/Notifikasi.tsx
+++ screens/Notifikasi.tsx
@@ -1,16 +1,47 @@
 import * as React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-export default function NotFoundScreen({ navigation }) {
+import DetailTransaksi from '../screens/DetailTransaksi';
+import DetailLainnya from '../screens/DetailLainnya';
+
+const Stack = createStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+export default function TabStack({ navigation }) {
     return (
-        <View style={styles.container}>
-            <Text>Hello</Text>
-        </View>
+        <Tab.Navigator
+            initialRouteName="TabNotifikasi"
+            tabBarOptions={{
+                activeTintColor: 'black',
+                fontWeight: '700',
+                inactiveTintColor: '#8BC34A',
+                style: {
+                    backgroundColor: '#FFFFFF',
+                },
+                labelStyle: {
+                    textAlign: 'center',
+                },
+                indicatorStyle: {
+                    borderBottomColor: '#87B56A',
+                    borderBottomWidth: 2,
+                },
+            }}>
+            <Tab.Screen
+                name="FirstPage"
+                component={DetailTransaksi}
+                options={{
+                    tabBarLabel: 'TRANSAKSI',
+                }} />
+            <Tab.Screen
+                name="SecondPage"
+                component={DetailLainnya}
+                options={{
+                    tabBarLabel: 'LAINNYA',
+
+                }} />
+        </Tab.Navigator>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-});
