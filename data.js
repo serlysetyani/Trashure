34fd05dbd1ec6449f45dc5cc3f3c1d8a09239a5b
serlyset <serlysetyani@student.telkomuniversity.ac.id>
2020-12-12 14:49:20 +0700

    coba data dummy

--- data.js
+++ data.js
@@ -1,28 +1,9 @@
-export const article = [
-{
-    id : 1,
-    judul : 'Cara Memilah Sampah yang Baik',
-    image :  '../assets/images/article1.png',
-    fullImage : '../assets/images/sampah.png'
-},
-{
-    id : 2,
-    judul : 'Cara Menggunakan Trashbag ID',
-    image : '../assets/images/article2.png',
-    fullImage : '../assets/images/sampah.png'
-},
-{
-    id: 3,
-    judul : 'Cara Membuang Sampah Mantan',
-    image : '../assets/images/article3.png',
-    fullImage : '../assets/images/sampah.png'
-}
-]
-
 export const profil =
 {
     nama : 'Serly Setyani',
-    nomorHp : '081234567890',
+    nomorHp : '081333518083',
     email : 'serlysetyani@trashure.com',
-    tanggalLahir : ''
+    password: 'mancingmania',
+    tanggalLahir: '09-06-2001',
+    saldo: 150000,
 }
